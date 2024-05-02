f3dae51fb2e241673d8dfc776c83b608344189b2
mjamilkahut <70695801+mjamilkahut@users.noreply.github.com>
2024-05-02 13:43:38 +1000

    SG.tf

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ resource "aws_instance" "Blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_group.ids = [aws_security_group.blog.id]
+  vpc_security_group_ids = [aws_security_group.blog.id]
 
   tags = {
     Name = "HelloWorld"
