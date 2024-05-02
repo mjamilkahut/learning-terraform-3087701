1d3ab63bcd1277f77189e9bd4d0730b37f35944c
mjamilkahut <70695801+mjamilkahut@users.noreply.github.com>
2024-05-02 13:41:38 +1000

    SG.tf

--- main.tf
+++ main.tf
@@ -29,14 +29,14 @@ resource "aws_instance" "Blog" {
   }
 }
 
-resourse "aws_security_group" "blog" {
+resource "aws_security_group" "blog" {
   name = "blog"
   description = "Allow Http and Https in. Allow all out"
 
   vpc_id = data.aws_vpc.default.id
 }
 
-resourse "aws_security_group_rule" "blog_http_in" {
+resource "aws_security_group_rule" "blog_http_in" {
   type = "ingress"
   from_port = 80
   to_port = 80
@@ -46,7 +46,7 @@ resourse "aws_security_group_rule" "blog_http_in" {
   security_group_id = aws_security_group.blog.id
 }
 
-resourse "aws_security_group_rule" "blog_https_in" {
+resource "aws_security_group_rule" "blog_https_in" {
   type = "ingress"
   from_port = 443
   to_port = 443
@@ -56,7 +56,7 @@ resourse "aws_security_group_rule" "blog_https_in" {
   security_group_id = aws_security_group.blog.id
 }
 
-resourse "aws_security_group_rule" "blog_all_out" {
+resource "aws_security_group_rule" "blog_all_out" {
   type = "egress"
   from_port = 0
   to_port = 0
